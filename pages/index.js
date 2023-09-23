bbb1678473a6024878b842a50ca62c03f9d083c8
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2023-09-23 13:37:22 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -129,7 +129,7 @@ export default function Home() {
               <h4 className="py-4 text-teal-500">Available On:-</h4>
               <p className="text-gray-800 py-1">LinkedIn</p>
               <p className="text-gray-800 py-1">Twitter</p>
-              <p className="text-gray-800 py-1">Gmail</p>
+              <p className="text-gray-800 py-1">Reddit</p>
               <p className="text-gray-800 py-1">Discord</p>
             </div>
           </div>
