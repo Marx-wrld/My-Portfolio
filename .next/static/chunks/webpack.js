1034ac50ced17a449d7714a43e3db296a68fbcfd
Marx-wrld <iongonga@kabarak.ac.ke>
2023-10-30 10:19:05 +0300

    json and .pack files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "eae981fc3910ece8"; }
+/******/ 		__webpack_require__.h = function() { return "919f10966c82e6d4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
