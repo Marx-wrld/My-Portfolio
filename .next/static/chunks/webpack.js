eab1f72c9a8a73dd23b95bd749cbe4f51307db6c
Marx-wrld <iongonga@kabarak.ac.ke>
2023-10-30 10:20:50 +0300

    Minor changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "91a46cae09b9cced"; }
+/******/ 		__webpack_require__.h = function() { return "0bd7cd03113d215a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.b9601f77e7b79b01.hot-update.js
+++ .next/static/webpack/webpack.b9601f77e7b79b01.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a05185a5c0348912"; }
+/******/ 	__webpack_require__.h = function() { return "0bd7cd03113d215a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
