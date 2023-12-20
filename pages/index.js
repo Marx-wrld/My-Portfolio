e6ad598e10c53e9f4653a20b31a994f21bb15c5c
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2023-12-20 22:23:19 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10 ">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+            <h2 className="text-5xl py-4 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Ian Marx
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
