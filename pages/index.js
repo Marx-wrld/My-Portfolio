fb70ab0cfaae9b8f73564c87fffa88f0ef59e199
Ian Marx <105711066+Marx-wrld@users.noreply.github.com>
2025-01-21 23:47:55 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-700 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="https://drive.google.com/file/d/1cUJ7DhgnUUzh-WoZSoia5LLHDKngvS9x/view?usp=sharing">
+                  href="https://drive.google.com/file/d/1Jbj8G7rxuO_aG32KfFQzMakjq26XGAKg/view?usp=sharing">
                   View Resume
                 </a>
               </li>
